76d070bbf4c44d4381d3ec0a2483697d79cf80f9
Adnan Anwar @Destkop <adnananwar85@gmail.com>
2024-02-05 10:54:31 +0800

    setup awal project

---
+++ src/main/java/tutorpzn/tutorspringboot/TutorSpringbootApplication.java
@@ -0,0 +1,13 @@
+package tutorpzn.tutorspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TutorSpringbootApplication.class, args);
+    }
+
+}
